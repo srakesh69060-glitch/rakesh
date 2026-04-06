225b3ab805086de9d39290d9a0b6b8dfb2627f87
Rakesh Sivalingam <srakesh69060@gmail.com>
2026-04-06 22:51:02 +0530

    fix render startup crash

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 
 
 class Config:
@@ -6,8 +6,9 @@ class Config:
     DB_USER = os.getenv("DB_USER", "ers_user")
     DB_PASSWORD = os.getenv("DB_PASSWORD", "Root@123")
     DB_NAME = os.getenv("DB_NAME", "emergency_response")
-    DB_PORT = int(os.getenv("DB_PORT", "3306"))
+    _DB_PORT_RAW = os.getenv("DB_PORT", "3306")
+    try:
+        DB_PORT = int(_DB_PORT_RAW)
+    except (TypeError, ValueError):
+        DB_PORT = 3306
     SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-change-me")
-
-
-
